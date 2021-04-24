03087600c452708d9103c5875eb6134aabbbd95d
Julien Hubert <3481141+Atlanta@users.noreply.github.com>
2021-04-24 17:24:58 +0200

    Restructured files

--- Assets/Scripts/GroundController.cs
+++ Assets/Scripts/GroundController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class TillemapController : MonoBehaviour
+public class GroundController : MonoBehaviour
 {
 
     public GameObject tileMapGameObject;
