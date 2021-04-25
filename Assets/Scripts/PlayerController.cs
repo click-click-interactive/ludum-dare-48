b2dc304327b9e19a570c29752734aea140468717
Julien Hubert <3481141+Atlanta@users.noreply.github.com>
2021-04-25 15:52:10 +0200

    player animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,26 +11,41 @@ public class PlayerController : MonoBehaviour
     public Canvas canvas;
     private GameObject hintInstance;
     private CallableAction actionTarget;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool moved = false;
+
         if (Input.GetKey(KeyCode.Z)) {
             transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.up, speed * Time.deltaTime);
+            moved = true;
+            animator.SetInteger("direction", (int) PlayerDirection.Up);
         } else if (Input.GetKey(KeyCode.S)) {
             transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.down, speed * Time.deltaTime);
+            moved = true;
+            animator.SetInteger("direction", (int) PlayerDirection.Down);
         }
 
         if (Input.GetKey(KeyCode.Q)) {
             transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.left, speed * Time.deltaTime);
+            moved = true;
+            animator.SetInteger("direction", (int) PlayerDirection.Left);
         } else if (Input.GetKey(KeyCode.D)) {
             transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.right, speed * Time.deltaTime);
+            moved = true;
+            animator.SetInteger("direction", (int) PlayerDirection.Right);
+        }
+
+        if (!moved) {
+            animator.SetInteger("direction", (int) PlayerDirection.None);
         }
 
         if (actionTarget != null && Input.GetKeyDown(KeyCode.F))
@@ -41,7 +56,7 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) {
         ActionTrigger actionTrigger = other.GetComponent<ActionTrigger>();
-        if (actionTrigger) {
+        if (actionTrigger && actionTrigger.action) {
             this.actionTarget = actionTrigger.action;
             hintPrefab.GetComponentInChildren<TMP_Text>().text = this.actionTarget.getType();
             hintInstance = Instantiate(
