f09377d2b96ed4f93d8c22a564f5caa7f9eb08c1
Raphaël Anic-Antic <raphael.anic-antic@polyconseil.fr>
2021-04-25 14:53:35 +0100

    save before merge

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,24 @@ using TMPro;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 3.0f;
-
+    public int health = 5;
+    public int damage = 3;
     public GameObject hintPrefab;
     public Canvas canvas;
     private GameObject hintInstance;
+    private GameObject triggerInstance;
+    private GameObject triggerEnemy;
     private CallableAction actionTarget;
+
+    private Renderer renderer;
+    public Color damageColor;
+    private Color originalColor;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        renderer = this.gameObject.GetComponent<SpriteRenderer>();
+        originalColor = renderer.material.color;
     }
 
     // Update is called once per frame
@@ -33,16 +41,24 @@ public class PlayerController : MonoBehaviour
             transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.right, speed * Time.deltaTime);
         }
 
-        if (actionTarget != null && Input.GetKeyDown(KeyCode.F))
+        if (actionTarget != null && Input.GetKeyDown(KeyCode.F) && triggerInstance != null)
         {
+            triggerInstance.SendMessage("StartConversation");
             actionTarget.call();
         }
+
+        if(Input.GetKeyDown(KeyCode.Space) && triggerEnemy != null)
+        {
+            triggerEnemy.SendMessage("receiveHit", damage);
+        } 
     }
 
     void OnTriggerEnter2D(Collider2D other) {
+
         ActionTrigger actionTrigger = other.GetComponent<ActionTrigger>();
         if (actionTrigger) {
             this.actionTarget = actionTrigger.action;
+            this.triggerInstance = other.gameObject;
             hintPrefab.GetComponentInChildren<TMP_Text>().text = this.actionTarget.getType();
             hintInstance = Instantiate(
                 hintPrefab,
@@ -51,15 +67,41 @@ public class PlayerController : MonoBehaviour
                 canvas.gameObject.transform
             );
         }
+
+        if(other.gameObject.tag == "Enemy")
+        {
+            triggerEnemy = other.gameObject;
+        }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
         ActionTrigger actionTrigger = other.GetComponent<ActionTrigger>();
         if (actionTrigger) {
+            if(this.triggerInstance == other.gameObject)
+            {
+                this.triggerInstance = null;
+            }
             if (this.hintInstance) {
                 Destroy(hintInstance);
             }
         }
+    }
+
+    public void receiveHit(int amount)
+    {
+        health -= amount;
+        Debug.Log("Player hp : " + health);
+        if (health <= 0)
+        {
+            Debug.Log("GAME OVER !");
+        }
+        renderer.material.color = damageColor;
+        Invoke("RestoreMaterial", 0.0f);
+    }
+
+    private void RestoreMaterial()
+    {
+        renderer.material.color = originalColor;
     }
 }
